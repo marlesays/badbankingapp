a4d546a538fad8c35630a99f4c64907f4b9dd479
Marle Says <marlesays@hotmail.com>
2024-02-06 17:06:45 -0500

    mongodb atlas string

--- dal.js
+++ dal.js
@@ -1,5 +1,41 @@
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://marlesays:<password>@atlascluster.uomnlq7.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+
+
+
+
+
+//
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
+const url         = "mongodb+srv://marlesays:<password>@atlascluster.uomnlq7.mongodb.net/?retryWrites=true&w=majority";
 // const url         = 'mongodb://localhost:5000';
 let db            = null;
  
